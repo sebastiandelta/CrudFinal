b892edc0edb278014e61f64c466a70700bdf5570
Juanscancelada <cancelada2003@gmail.com>
2024-10-09 12:02:27 -0500

    histories

--- database/migrations/2024_09_20_161346_create_histories_table.php
+++ database/migrations/2024_09_20_161346_create_histories_table.php
@@ -4,23 +4,20 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
+class CreateHistoriesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
-        
-    }
-
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::table('histories', function (Blueprint $table) {
-            $table->dropColumn('precio');
+        Schema::create('histories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('item_id')->constrained()->onDelete('cascade'); 
+            $table->string('descripcion');
+            $table->timestamps();
         });
     }
-};
+
+    public function down()
+    {
+        Schema::dropIfExists('histories');
+    }
+}
